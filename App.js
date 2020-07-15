b0ab442a95d441a63d0fe743c157bcfc6824588a
MirLih <alexandre.diniz.dias@gmail.com>
2020-07-15 13:14:57 -0300

    header

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={{ flexDirection: 'row', backgroundColor: '#000', alignItems:'center', justifyContent:'space-around' }} >
+        <Image source={{ uri: "https://br.goodwe.com/Public/Home/images/logo.png" }} style={{ height: 60, width: 145, resizeMode: 'contain' }} />
+        <Text style={{color:'#FFF'}} > Aplicação Comercial </Text>
+      </View>
+      <View>
+        <Text> Busca </Text>
+      </View>
+      <View>
+        <Text> Body </Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
